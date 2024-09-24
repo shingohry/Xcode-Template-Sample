d6660728566f9b37c42f0cbf55f0bd9d3bf9d2c2
Shingo Hiraya <shingo.318@studist.jp>
2024-09-24 15:37:13 +0900

    Initial Commit

---
+++ TemplateSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TemplateSample
+//
+//  Created by Shingo Hiraya on 2024/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TemplateSample/TemplateSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemplateSampleApp.swift
+//  TemplateSample
+//
+//  Created by Shingo Hiraya on 2024/09/20.
+//
+
+import SwiftUI
+
+@main
+struct TemplateSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
